cea89d5ae0bbf7c67635e8f7ff570d6d3b1d57e1
MoriaNuri <moriakorsia@gmail.com>
2021-07-14 14:01:23 +0300

    img folder

--- src/views/home-page.vue
+++ src/views/home-page.vue
@@ -2,8 +2,8 @@
   <div class="home-page">
     <hero></hero>
     <div>
-       <h5>Inspiration destinations</h5>
-      <div class="cities">
+       <h5>Live anywhere</h5>
+      <div class="categories">
         <div clas="city">
       <router-link  to="/"> <img :src="require('@/assets/img/1.png')" /></router-link>
          <h4>New York</h4>
@@ -18,6 +18,28 @@
         </div>
         <div clas="city">
       <router-link  to="/"> <img :src="require('@/assets/img/4.png')" /></router-link>
+       <h4>Amsterdam</h4>
+        </div>
+          </div>
+
+    </div>
+    <div>
+       <h5>Inspiration destinations</h5>
+      <div class="cities">
+        <div clas="city">
+      <router-link  to="/"> <img :src="require('@/assets/img/home-page/1.png')" /></router-link>
+         <h4>New York</h4>
+        </div>
+         <div clas="city">
+      <router-link  to="/"> <img :src="require('@/assets/img/home-page/2.png')" /></router-link>
+      <h4>Tel-Aviv</h4>
+        </div>
+          <div clas="city">
+      <router-link  to="/"> <img :src="require('@/assets/img/home-page/3.png')" /></router-link>
+       <h4>Paris</h4>
+        </div>
+        <div clas="city">
+      <router-link  to="/"> <img :src="require('@/assets/img/home-page/4.png')" /></router-link>
        <h4>Amsterdam</h4>
         </div>
           </div>
